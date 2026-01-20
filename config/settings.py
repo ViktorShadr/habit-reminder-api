147b8818eeecea9b052fd2c5e138675dbd71347a
ViktorShadr <v.viktor.shadrin@gmail.com>
2026-01-20 14:45:10 +0300

    feat: add CORS support and API pagination
    - Add django-cors-headers dependency and middleware
    - Configure CORS for local development (localhost:8000, 127.0.0.1:8000)
    - Enable default pagination for DRF with page size of 5
    - Improve API usability for frontend integration

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "users",
     "habits",
     "drf_yasg",
+    "corsheaders",
 ]
 
 REST_FRAMEWORK = {
@@ -44,6 +45,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
 }
 
 SIMPLE_JWT = {
@@ -59,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -119,3 +123,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
