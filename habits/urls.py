8b6da2b911bd0b37d600f10389626b53eeb87614
Viktor Shadrin <v.viktor.shadrin@gmail.com>
2026-02-10 15:05:37 +0300

    fix(habits): set script prefix from FORCE_SCRIPT_NAME in urls

--- habits/urls.py
+++ habits/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.urls import set_script_prefix
 
 from habits.apps import HabitsConfig
 from habits.views import HabitViewSet, PublicListAPIView
+
+if getattr(settings, "FORCE_SCRIPT_NAME", None):
+    set_script_prefix(settings.FORCE_SCRIPT_NAME)
 
 app_name = HabitsConfig.name
 
