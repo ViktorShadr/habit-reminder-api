412c7cf98affb3829d1536cd7290841363d1084b
Viktor Shadrin <v.viktor.shadrin@gmail.com>
2026-02-10 14:55:44 +0300

    feat: support /habit prefix behind reverse proxy • enable FORCE_SCRIPT_NAME and USE_X_FORWARDED_HOST in settings • apply set_script_prefix when FORCE_SCRIPT_NAME is set • pass FORCE_SCRIPT_NAME via docker-compose env

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,11 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.conf import settings
+from django.urls import set_script_prefix
+
+if getattr(settings, "FORCE_SCRIPT_NAME", None):
+    set_script_prefix(settings.FORCE_SCRIPT_NAME)
 
 schema_view = get_schema_view(
     openapi.Info(
